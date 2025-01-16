4d34058e138c595d25923b3f63bf4ca7a27f21af
23048180-yongkeong <23048180@myrp.edu.sg>
2025-01-16 15:52:50 +0800

    Implement searching of items from an API.

--- App.js
+++ App.js
@@ -1,20 +1,87 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState, useEffect} from 'react';
+import { ToastAndroid, View, Text, StatusBar, TextInput, FlatList, StyleSheet} from 'react-native';
 
-export default function App() {
-  return (
+let originalIdioms = [];
+
+const App = () => {
+  const [idioms, setIdioms] = useState([]);
+
+  useEffect(() => {
+    fetch("https://mysafeinfo.com/api/data?list=americanidioms&format=json&case=default")
+        .then((response) => {
+          return response.json();
+        })
+        .then((myJson) => {
+          if (originalIdioms.length === 0) {
+            setIdioms(myJson);
+            originalIdioms = myJson;
+          }
+        });
+  }, []);
+
+  const FilterIdiom = (text) => {
+    if (text!= "") {
+      let searchIdiom = originalIdioms.filter((item) =>
+          item.Idiom.includes(text));
+      if (searchIdiom.length > 0) {
+        setIdioms(searchIdiom);
+      }
+      else {
+        setIdioms([]);
+        ToastAndroid.show("Idiom result not found!", ToastAndroid.SHORT);
+      }
+    }
+    else {
+      setIdioms(originalIdioms);
+    }
+  }
+
+  const renderItem = ({item}) => {
+    return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.textStyles}>Idiom: {item.Idiom}</Text>
+      <Text style={styles.textStyles}>Meaning: {item.Notes}</Text>
+    </View>
+    );
+  };
+
+  return (
+    <View style={{backgroundColor: 'red', flex: 1}}>
+      <StatusBar/>
+      <Text style={[styles.headerText]}>American Idioms</Text>
+      <Text style={styles.textStyles}>Search:</Text>
+      <TextInput
+          style={styles.inputStyles}
+          placeholder={"Seach an idiom"}
+          onChangeText={(text) => {FilterIdiom(text)}}/>
+      <FlatList data={idioms} renderItem={renderItem} />
     </View>
   );
 }
 
+export default App;
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'column',
+    borderWidth: 1,
+    margin: 10,
+    backgroundColor: 'yellow'
   },
+  textStyles: {
+    fontSize: 15,
+    margin: 10
+  },
+  headerText: {
+    fontSize: 30,
+    padding: 10,
+    textAlign: 'center',
+    backgroundColor: 'skyblue'
+  },
+  inputStyles: {
+    margin: 10,
+    borderWidth: 1,
+    fontSize: 15
+  }
 });
